cacb6a617f50048e437a290c25b99f68d471b40c
alexbakhu <aleqsandrekepler123@gmail.com>
2025-03-01 15:17:53 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,102 @@
+let name1 = "Mike"
+let name2 = "Bakhu"
+
+console.log(name2);
+
+let num1 = 45
+
+let num2 = -45
+
+let num3 = 12 + 15
+
+let num4 = 12 * 15
+
+let num5 = 15 / 15
+
+let num6 =  15 - 12
+
+console.log(num1)
+console.log(num2)
+console.log(num3)
+console.log(num4)
+console.log(num5)
+console.log(num6)
+
+let age1 = 12
+
+num1 = 47
+
+console.log(age1)
+
+let color1 = "black"
+
+color1 = "blue"
+
+console.log(color1)
+
+let s = "hi my name's  Bakhu"
+
+console.log(s)
+
+let h2 = document.querySelector(".hello-h2")
+
+console.log(h2)
+
+h2.style.color = "black"
+h2.style.background = "dark green"
+h2.style.border = '2px solid BLUE'
+h2.style.padding - '20px'
+h2.style.borderRadius = "20px"
+h2.style.width = '100px'
+h2.style.height = "100px"
+
+h2.remove()
+
+// davaleba 
+
+let button = document.querySelector(".btn1")
+
+button.style.color = "black"
+button.style.background = "green"
+button.style.border = '10px solid darkGreen'
+button.style.padding - '15px'
+button.style.borderRadius = "14px"
+button.style.width = '100px'
+button.style.height = "50px"
+
+// davaleba 2
+
+let input = document.querySelector('.inp-1')
+
+    console.log(input)
+
+input.style.color = "black"
+input.style.background = "green"
+input.style.border = '19px solid blue'
+input.style.padding - '19px'
+input.style.borderRadius = "100%"
+input.style.width = '60px'
+input.style.height = "75px"
+
+// rogor gavaqrot roca gilaks davawert
+
+let btn2 = document.querySelector(".btn2")
+let p2 = document.querySelector(".p2")
+
+    console.log(btn2)
+    console.log(p2)
+
+
+btn2.addEventListener('click', () => {
+
+    console.log('user clicked  on button')
+
+
+})
+
+p2.addEventListener('click', () => {
+
+    p2.remove()
+
+
+})
